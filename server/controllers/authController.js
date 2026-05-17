1dfc51222c6647bbc79f3519da3075ae1a2b2747
Harshithashetty111 <shettyharshithat@gmail.com>
2026-05-17 22:31:48 +0530

    Fix Nodemailer transporter config to use service: 'gmail'

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -118,10 +118,7 @@ const sendOtp = async (req, res) => {
     otpStore.set(email, { otp, expiresAt });
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
