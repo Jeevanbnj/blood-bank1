29fd43b64b52c98feec85761a6fbaab38974ac6c
Harshithashetty111 <shettyharshithat@gmail.com>
2026-05-17 23:02:58 +0530

    Fix Brevo SMTP

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -118,12 +118,14 @@ const sendOtp = async (req, res) => {
     otpStore.set(email, { otp, expiresAt });
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      }
-    });
+  }
+});
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
