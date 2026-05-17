164ec8b65a2c6c94e6674fc27f3d3bd07755c0ae
Harshithashetty111 <shettyharshithat@gmail.com>
2026-05-17 22:02:31 +0530

    fix axios baseURL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-baseURL = baseURL.replace(/\\/+$/, '');
+baseURL = baseURL.replace(/\/+$/, '');
 if (!baseURL.endsWith('/api')) {
   baseURL = `${baseURL}/api`;
 }
